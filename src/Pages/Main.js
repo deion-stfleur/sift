1bea891f8cd3f93a85c0ca47bd6c9bb2a67278e2
deion-stfleur <deionstfleur13@gmail.com>
2024-09-21 14:15:29 -0400

    auth test
    test

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,26 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState,  useEffect} from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import './Main.css'
 import axios from 'axios';
+import { gapi } from 'gapi-script';
 
+const clientID = "5321516054-qje2dkbjrq4133ke4o18aifipr3n7gvr.apps.googleusercontent.com"
 
 function Main() {
 
     const [tokenResponse, setTokenResponse] = useState(null);
     const handleLoginSuccess = async (credentialResponse) => {
-        const token = credentialResponse.credential;
-        const response = await fetch('https://openidconnect.googleapis.com/v1/userinfo?access_token=' + token);
-        const userData = await response.json();
-        console.log("Login Successful with user data:", userData);
-        console.log(token);
-        setTokenResponse(token);
-        alert(`Signed in as ${userData.email} and ${token}`);
-        // window.location.href = '/confirmation';
+      console.log("Log in Successful! Current user:", credentialResponse.profileObj);
     };
     
     const handleLoginError = () => {
         console.log('Login Failed');
     };
+
+    useEffect(() => {
+        function start() {
+            gapi.client.init({
+                clientID: clientID,
+                scope: ""
+            })
+        }
+
+        gapi.load('client:auth2', start)
+    })
+
+    var accessToken = gapi.auth && gapi.auth.getToken() ? gapi.auth.getToken().access_token : null;
+
+    
+
+
   return (
 
     <>
@@ -34,13 +46,14 @@ function Main() {
                 <GoogleLogin
                 onSuccess={handleLoginSuccess}
                 onError={handleLoginError}
-                
+                cookiePolicy={'single_host_origin'}
                 />
                 {/* <div className='main-btn'>
                     <p className='main-text'>Sign in with Google</p>
                 </div> */}
                 </GoogleOAuthProvider>
                 <p>{tokenResponse}</p>
+                <p>{accessToken}</p>
             </div>
     </div>
     </>
