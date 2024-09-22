888e2c1f962c00dca1912c0fbd546ec8f581d957
deion-stfleur <deionstfleur13@gmail.com>
2024-09-22 18:34:37 -0400

    test with email
    test

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -14,7 +14,7 @@ function Main() {
       const accessToken = credentialResponse.credential;
       console.log("Access Token: ", accessToken);
       try {
-        const response = await axios.post('http://127.0.0.1:5000/api/get_emails', { token: accessToken });
+        const response = await axios.post('https://email-backend-project-6e9ab27b2095.herokuapp.com/api/get_emails', { token: accessToken });
         console.log(response.data);
         window.location.href="/confirmation"
       } catch (error) {
@@ -40,7 +40,7 @@ function Main() {
              <p className='main-copy'>We help you discover better alternatives for the products you buy, while saving you money.</p>
             <div className='main-btn'>
 
-                <GoogleOAuthProvider clientId='5321516054-qje2dkbjrq4133ke4o18aifipr3n7gvr.apps.googleusercontent.com'>
+                <GoogleOAuthProvider  style={{backgroundColor: 'white', color: 'black', width: '220px', borderRadius: '200px'}} clientId='5321516054-qje2dkbjrq4133ke4o18aifipr3n7gvr.apps.googleusercontent.com'>
                 <GoogleLogin
                 onSuccess={handleLoginSuccess}
                 onError={handleLoginError}
